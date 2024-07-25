39a2c541760af72d2b69ba59f54e4e75b5169975
mush42 <musharraf.omer@gmail.com>
2024-07-25 19:49:26 +0200

    Fixed inference of duration predictor

--- optispeech/model/generator/modules.py
+++ optispeech/model/generator/modules.py
@@ -219,11 +219,8 @@ class DurationPredictor(torch.nn.Module):
         """
         log_durations = self(x, mask)
         # linear domain
-        durations = (torch.exp(log_durations) - self.clip_val)
-        durations = torch.ceil(durations * factor)
-        # avoid negative values
-        durations = torch.clamp(durations.long(), min=0) 
-        durations = durations.masked_fill(mask, 0)
+        durations = torch.clamp(log_durations.exp() - self.clip_val, min=0)
+        durations = durations.masked_fill(mask, 0.0)
         return durations
 
 
