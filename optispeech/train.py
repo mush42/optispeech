d9b4953219a34a28ee77d5019615119a72a5e5aa
mush42 <musharraf.omer@gmail.com>
2024-09-10 00:44:51 +0200

    Finalized forced_resume feature

--- optispeech/train.py
+++ optispeech/train.py
@@ -75,9 +75,11 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         utils.log_hyperparameters(object_dict)
 
     ckpt_path = cfg.get("ckpt_path")
-    if (ckpt_path is not None) and cfg.get("resume_from_partial_match"):
+    if (ckpt_path is not None) and cfg.get("forced_resume"):
         model_cls = type(model)
-        model = model_cls.load_from_checkpoint(ckpt_path, map_location="cpu", strict=False, **model.hparams)
+        ckpt_model = model_cls.load_from_checkpoint(ckpt_path, map_location="cpu")
+        model.load_state_dict(ckpt_model.state_dict(), strict=False)
+        del model_cls, ckpt_model
         ckpt_path = None
 
     if cfg.get("train"):
